6dfea0270770d7a5f4c6708706f4d0faebfc93cd
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-09 15:40:09 +0100

    Create FlywayConfig.java

---
+++ src/main/java/com/shotaroi/featureflags/config/FlywayConfig.java
@@ -0,0 +1,19 @@
+package com.shotaroi.featureflags.config;
+
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+    }
+}
