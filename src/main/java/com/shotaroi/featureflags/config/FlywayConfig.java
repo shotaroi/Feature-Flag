645868ab50668ad48da036cd1ed987b225b184f4
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-09 16:18:29 +0100

    Update FlywayConfig.java

--- src/main/java/com/shotaroi/featureflags/config/FlywayConfig.java
+++ src/main/java/com/shotaroi/featureflags/config/FlywayConfig.java
@@ -1,6 +1,7 @@
 package com.shotaroi.featureflags.config;
 
 import javax.sql.DataSource;
+
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
